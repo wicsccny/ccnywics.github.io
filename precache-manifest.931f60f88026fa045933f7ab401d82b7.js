3d9f6de2142c1745d7c3be589ef3a5baff92ceb0
Abigail <abanting00@gmail.com>
2019-08-31 23:00:45 -0400

    Updates

---
+++ precache-manifest.931f60f88026fa045933f7ab401d82b7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc2e4aa670e9d7970d8435e959231e2b",
+    "url": "/wics/index.html"
+  },
+  {
+    "revision": "01507e63324bbfe8f6a7",
+    "url": "/wics/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "32f2e661feaa57fcac25",
+    "url": "/wics/static/css/main.dc9454e6.chunk.css"
+  },
+  {
+    "revision": "01507e63324bbfe8f6a7",
+    "url": "/wics/static/js/2.562d5930.chunk.js"
+  },
+  {
+    "revision": "32f2e661feaa57fcac25",
+    "url": "/wics/static/js/main.fd316d0c.chunk.js"
+  },
+  {
+    "revision": "9ea3cec1ee97f80b1344",
+    "url": "/wics/static/js/runtime~main.4f3144ea.js"
+  },
+  {
+    "revision": "1467ea6b8c12dcb01ad62ace6ec9797b",
+    "url": "/wics/static/media/wicsLogoDark.1467ea6b.png"
+  },
+  {
+    "revision": "8ac5b4add5a357395a5e27c3b3e44e87",
+    "url": "/wics/static/media/wicsLogoLight.8ac5b4ad.png"
+  }
+]);
